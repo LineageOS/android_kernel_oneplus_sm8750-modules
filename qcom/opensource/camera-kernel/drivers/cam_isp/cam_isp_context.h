47af83d01df1e2e04f7a2dc0038360e9ed739d06
Bruno Martins <bgcngm@gmail.com>
2026-04-24 22:51:29 +0100

    camera-kernel: cam_isp: Remove dead code
    Avoids conflicts with latest CLO tag merge.
    
    Change-Id: I1840237a0d78150ef8c7e6cd07aa1674bfbaff8e

--- qcom/opensource/camera-kernel/drivers/cam_isp/cam_isp_context.h
+++ qcom/opensource/camera-kernel/drivers/cam_isp/cam_isp_context.h
@@ -428,7 +428,6 @@ struct cam_isp_fcg_prediction_tracker {
  *                             by other devices on the link as part of link setup
  * @mode_switch_en:            Indicates if mode switch is enabled
  * @sfe_en:                    Indicates if SFE is being used
- * @last_apply_settings:       Indicates last applied settings is default or valid req setting
  *                             true is default and false is valid
  *
  */
@@ -500,7 +499,6 @@ struct cam_isp_context {
 	bool                                  mode_switch_en;
 	bool                                  sfe_en;
 	bool                                  standby_en;
-	bool                                  last_apply_settings;
 };
 
 /**
