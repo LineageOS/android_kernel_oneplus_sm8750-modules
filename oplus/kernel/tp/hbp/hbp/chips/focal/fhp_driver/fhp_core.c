a68d5aa1387b718f4dc0f8fb672524427a7550fb
NullCode1337 <contact.nullcode@proton.me>
2026-09-19 20:38:53 +0600

    oplus: tp: hbp: Unconditionally build the modules
    Change-Id: I735eebfc9b3e289a1860080220f9f24e04bd5414

--- oplus/kernel/tp/hbp/hbp/chips/focal/fhp_driver/fhp_core.c
+++ oplus/kernel/tp/hbp/hbp/chips/focal/fhp_driver/fhp_core.c
@@ -543,7 +543,7 @@ static struct platform_driver fts_dev_driver = {
 	},
 };
 
-static int __init fts_platform_init()
+static int __init fts_platform_init(void)
 {
 	return platform_driver_register(&fts_dev_driver);
 }
